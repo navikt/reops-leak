5f2717fc4edce474ed354b71df0571adf376194e
EIlif Johansen <eilifjohansen@outlook.com>
2024-09-22 13:24:00 +0200

    Språkfiks

--- src/components/mixed/akselalert.tsx
+++ src/components/mixed/akselalert.tsx
@@ -3,7 +3,7 @@ import { Alert } from "@navikt/ds-react";
 const akselalert = () => {
     return (
         <Alert variant="success" className="mt-0 mb-8 max-w-fit">
-            <p>Besøket ditt har blitt logget til Amplitude og Umami</p>
+            <p>Besøket har blitt logget til Amplitude og Umami</p>
         </Alert>
     );
 };
