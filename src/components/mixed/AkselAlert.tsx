b74c40c5cffe2ab0b3cebabee8000996ab5208bb
EIlif Johansen <eilifjohansen@outlook.com>
2024-09-23 15:42:22 +0200

    Fikset AkselAlert komponentet

--- src/components/mixed/AkselAlert.tsx
+++ src/components/mixed/AkselAlert.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import { Alert, Heading } from "@navikt/ds-react";
+import { Alert } from "@navikt/ds-react";
 
 interface Props {
     variant?: 'success' | 'info' | 'warning' | 'error';
@@ -7,25 +6,9 @@ interface Props {
 }
 
 export default function AkselAlert({ variant = "success", children }: Props) {
-    const [query, setQuery] = useState<string | null>(null);
-
-    useEffect(() => {
-        const searchParams = new URLSearchParams(window.location.search);
-        const q = searchParams.get('q');
-        setQuery(q);
-    }, []);
-
-    if (!query) {
-        return null; // Don't render anything if there's no query
-    }
-
     return (
         <Alert variant={variant} className="mt-0 mb-8 max-w-md">
-            <Heading spacing size="small" level="3">
-                Logget til Amplitude og Umami
-            </Heading>
-            URL-parameteren "q" er satt til "{query}".
-            {/*{children}*/}
+            {children}
         </Alert>
     );
 }
