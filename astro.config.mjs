2451e14ee9f26470108b4d0efd7c447e600510b0
EIlif Johansen <eilifjohansen@outlook.com>
2024-09-24 09:51:56 +0200

    Tester htmx

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import htmx from 'astro-htmx';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind({
         applyBaseStyles: false,
-    }), react()],
+    }), react(), htmx()],
     site: 'https://leak.ansatt.nav.no',
     output: 'hybrid',
     adapter: node({
